49e7b77d6fe3c8999cc6f594648bb87afa2b00d8
AlexForJava <hqq@ukr.net>
2019-03-31 23:28:43 +0300

    third commit

--- src/test/java/com/gmail/chernii/oleksii/FruitStoreTest.java
+++ src/test/java/com/gmail/chernii/oleksii/FruitStoreTest.java
@@ -2,6 +2,7 @@ package com.gmail.chernii.oleksii;
 
 
 import junit.framework.TestCase;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
@@ -9,80 +10,100 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class FruitStoreTest extends TestCase {
-    private FruitStore fruitStore = new FruitStore();
+
 
     @Test
-    public void testAddFruits() throws Exception {
-        fruitStore.addFruits("json/firstSupply.json");
-        fruitStore.addFruits("json/secondSupply.json");
-        assertFalse(fruitStore.getFruits().isEmpty());
+    public void testAddFruits() {
+        FruitStore localFruitStore = new FruitStore();
+        localFruitStore.addFruits("json/firstSupply.json");
+        localFruitStore.addFruits("json/secondSupply.json");
+        assertFalse(localFruitStore.getFruits().isEmpty());
     }
 
     @Test
     public void testSave() throws Exception {
-        fruitStore.addFruits("json/firstSupply.json");
-        fruitStore.addFruits("json/secondSupply.json");
-        fruitStore.save("json/data.json");
+        FruitStore localFruitStore = new FruitStore();
+        Fruit fruit = new Fruit(FruitType.BANANA, 10, new Date(), 20);
+        localFruitStore.add(fruit);
+        localFruitStore.save("json/data.json");
         File file = new File("json/fruitStore.json");
         assertTrue(file.exists());
     }
 
     @Test
-    public void testLoad() throws Exception {
+    public void testLoad() {
         FruitStore localFruitStore = new FruitStore();
         localFruitStore.load("json/data.json");
         assertFalse(localFruitStore.getFruits().isEmpty());
     }
 
     @Test
-    public void testGetAvailableFruits() throws Exception {
-        fruitStore.load("json/data.json");
+    public void testGetAvailableFruits() {
+        FruitStore localFruitStore = new FruitStore();
+        Fruit banana = new Fruit(FruitType.BANANA, 10, new Date(), 20);
+        Fruit avocado = new Fruit(FruitType.AVOKADO, 5, new Date(), 20);
+        localFruitStore.add(banana);
+        localFruitStore.add(avocado);
         Calendar calendar = Calendar.getInstance();
-        calendar.set(2019, 3, 31);
+        calendar.set(2019, 3, 8);
         Date date = calendar.getTime();
-        assertEquals(fruitStore.getAvailableFruits(date).size(), 0);
+        assertEquals(localFruitStore.getAvailableFruits(date).size(), 1);
     }
 
     @Test
-    public void testGetSpoiledFruits() throws Exception {
-        fruitStore.load("json/data.json");
+    public void testGetSpoiledFruits() {
+        FruitStore localFruitStore = new FruitStore();
+        Fruit banana = new Fruit(FruitType.BANANA, 10, new Date(), 20);
+        Fruit avocado = new Fruit(FruitType.AVOKADO, 5, new Date(), 20);
+        localFruitStore.add(banana);
+        localFruitStore.add(avocado);
         Calendar calendar = Calendar.getInstance();
-        calendar.set(2019, 2, 31);
+        calendar.set(2019, 3, 8);
         Date date = calendar.getTime();
-        assertEquals(fruitStore.getSpoiledFruits(date).size(), 5);
+        assertEquals(localFruitStore.getSpoiledFruits(date).size(), 1);
     }
 
     @Test
-    public void testGetSpoiledFruitsType() throws Exception {
-        fruitStore.load("json/data.json");
+    public void testGetSpoiledFruitsType() {
+        FruitStore localFruitStore = new FruitStore();
+        Fruit banana = new Fruit(FruitType.BANANA, 10, new Date(), 20);
+        Fruit avocado = new Fruit(FruitType.AVOKADO, 5, new Date(), 20);
+        localFruitStore.add(banana);
+        localFruitStore.add(avocado);
         Calendar calendar = Calendar.getInstance();
-        calendar.set(2019, 2, 31);
+        calendar.set(2019, 3, 8);
         Date date = calendar.getTime();
-        assertEquals(fruitStore.getSpoiledFruits(date, FruitType.CHERRY).size(), 1);
+        assertEquals(localFruitStore.getSpoiledFruits(date, FruitType.BANANA).size(), 0);
+        assertEquals(localFruitStore.getSpoiledFruits(date, FruitType.AVOKADO).size(), 1);
     }
 
     @Test
-    public void testGetAddedFruits() throws Exception {
-        fruitStore.load("json/data.json");
+    public void testGetAddedFruits() {
+        FruitStore localFruitStore = new FruitStore();
+        Fruit banana = new Fruit(FruitType.BANANA, 10, new Date(), 20);
+        Fruit avocado = new Fruit(FruitType.AVOKADO, 5, new Date(), 20);
+        localFruitStore.add(banana);
+        localFruitStore.add(avocado);
         Calendar calendar = Calendar.getInstance();
-        calendar.set(2019, 2, 12);
+        calendar.set(2019, 3, 8);
         Date date = calendar.getTime();
-        assertEquals(fruitStore.getAddedFruits(date).size(), 5);
+        assertEquals(localFruitStore.getAddedFruits(new Date()), 2);
+        assertFalse(localFruitStore.getAddedFruits(date).isEmpty());
+    }
+
+    @Test
+    public void testGetAddedFruitsType() {
+        FruitStore localFruitStore = new FruitStore();
+        Fruit banana = new Fruit(FruitType.BANANA, 10, new Date(), 20);
+        Fruit avocado = new Fruit(FruitType.AVOKADO, 5, new Date(), 20);
+        localFruitStore.add(banana);
+        localFruitStore.add(avocado);
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2019, 3, 8);
+        Date date = calendar.getTime();
+        assertEquals(localFruitStore.getAddedFruits(date, FruitType.BANANA).size(), 1);
         calendar.add(Calendar.DAY_OF_MONTH, 1);
         date = calendar.getTime();
-        System.out.println(date+ " - " + fruitStore.getAddedFruits(date).size());
-        assertTrue(fruitStore.getAddedFruits(date).isEmpty());
-    }
-
-    @Test
-    public void testGetAddedFruitsType() throws Exception {
-        fruitStore.load("json/data.json");
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(2019, 2, 12);
-        Date date = calendar.getTime();
-        assertEquals(fruitStore.getAddedFruits(date, FruitType.BANANA).size(), 1);
-        calendar.add(Calendar.DAY_OF_MONTH, 1);
-        date = calendar.getTime();
-        assertTrue(fruitStore.getAddedFruits(date, FruitType.BANANA).isEmpty());
+        assertTrue(localFruitStore.getAddedFruits(date, FruitType.BANANA).isEmpty());
     }
 }
