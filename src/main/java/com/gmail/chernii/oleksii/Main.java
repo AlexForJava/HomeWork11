9f0d5fce33a79bb0c19166cd28154471008774f0
AlexForJava <hqq@ukr.net>
2019-03-31 19:40:05 +0300

    first commit

--- src/main/java/com/gmail/chernii/oleksii/Main.java
+++ src/main/java/com/gmail/chernii/oleksii/Main.java
@@ -1,26 +1,17 @@
 package com.gmail.chernii.oleksii;
 
 
-import java.io.File;
-import java.util.Calendar;
-import java.util.Date;
-
 /**
  * Created by Space on 27.03.2019.
  */
 public class Main {
     public static void main(String[] args) {
-        /*FruitStore fruitStore = new FruitStore();
-        load(fruitStore);
-        System.out.println(fruitStore.getFruits().size());
-*/
-        File file = new File("json/fruitStore.json");
-        System.out.println(file.exists());
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(2019, 2, 31);
-        Date date = calendar.getTime();
-        System.out.println(date);
+        FruitStore fruitStore = new FruitStore();
+        addAndSave(fruitStore);
 
+        fruitStore.getFruits().clear();
+        load(fruitStore);
+        fruitStore.getFruits().forEach(System.out::println);
     }
 
     public static void addAndSave(FruitStore fruitStore) {
